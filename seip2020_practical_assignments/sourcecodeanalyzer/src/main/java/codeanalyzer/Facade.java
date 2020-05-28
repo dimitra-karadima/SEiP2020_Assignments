541f0f96499f57f199b42d04d6c51f1bff3e5de0
dimitra-karadima <t8170038@aueb.gr>
2020-05-28 15:20:32 +0300

    Change Facade class
    Change argument in order for the DemoClient to run as it should.

--- seip2020_practical_assignments/sourcecodeanalyzer/src/main/java/codeanalyzer/Facade.java
+++ seip2020_practical_assignments/sourcecodeanalyzer/src/main/java/codeanalyzer/Facade.java
@@ -8,8 +8,8 @@ public class Facade {
 	
 	public void operation(String filepath, String sourceCodeAnalyzerType, String sourceFileLocation, String outputFilePath, String outputFileType) throws IOException{
 		AnalyzerTypeFactory analyzerFactory = new AnalyzerTypeFactory();
-		AnalyzerType analyzer = analyzerFactory.createAnalyzerType(sourceFileLocation);
-		SourceCodeAnalyzer source = new SourceCodeAnalyzer(sourceCodeAnalyzerType);
+		AnalyzerType analyzer = analyzerFactory.createAnalyzerType(sourceCodeAnalyzerType);
+		SourceCodeAnalyzer source = new SourceCodeAnalyzer(sourceFileLocation);
 		int loc = analyzer.calculateLOC(filepath, source.getFileReader());
 		int nom = analyzer.calculateNOM(filepath, source.getFileReader());
 		int noc = analyzer.calculateNOC(filepath, source.getFileReader());
